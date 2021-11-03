de6a779c9fa759e4846974a6addf6a8f7dba7105
jaino <msb3302@naver.com>
2021-11-03 15:56:24 +0900

    compose Ch1 List와 Animatation, Layout

--- week 1-Jetpack Compose basics/app/src/main/java/com/example/basiccodelab1/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/basiccodelab1/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.basiccodelab1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelab1.ui.theme.BasicCodeLab1Theme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.primarySurface
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent { //xml 대신에 compose 함수가 들어감
             BasicCodeLab1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) { // surface = background
-                   messageCard(Message("ANDROID","jetpack compose"))
-                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -41,20 +48,51 @@ fun messageCard(msg: Message){
             modifier = Modifier
                 .size(48.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = msg.author)
+        //state가 변경될 때 remember 컴포저블로 객체를 저장 수정할 때 mutableStateOf<T>로 접근
+        var isExpanded by remember{ mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable{ isExpanded = !isExpanded}){
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+            color = surfaceColor, modifier = Modifier.animateContentSize().padding(1.dp)) {
+                Text(
+                   text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
-
-@Preview(showBackground = true)
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages) { message ->
+            messageCard(message)
+        }
+    }
+}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
-    messageCard(
-        msg = Message("jaino", "hello world")
-    )
+    BasicCodeLab1Theme {
+        Conversation(SampleData.conversationSample)
+    }
 }
