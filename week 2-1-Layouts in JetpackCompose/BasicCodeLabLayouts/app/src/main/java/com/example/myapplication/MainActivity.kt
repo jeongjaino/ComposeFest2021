1b1f2cf517b74d24fe9a585da3c4f855a8274058
jaino <msb3302@naver.com>
2021-11-10 16:02:38 +0900

    Custom Layout, Layout modifiers under the hood

--- week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.text.method.CharacterPickerDialog
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
@@ -14,13 +15,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.launch
@@ -30,7 +35,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                ImageList()
+                BodyContent()
             }
         }
     }
@@ -49,7 +54,7 @@ fun SimpleList(){
 fun LazyList(){
 
     val scrollState = rememberLazyListState()
-    
+
     LazyColumn(state = scrollState){
         items(100){
             Text("Item $it")
@@ -72,14 +77,7 @@ fun LayoutCodeLab(){
             )
         }
     ) { innerPadding ->
-           BodyContent(Modifier.padding(innerPadding))
-    }
-}
-@Composable
-fun BodyContent(modifier : Modifier = Modifier ){
-    Column(modifier = modifier.padding(8.dp)){
-        Text(text = "안녕하세요!")
-        Text(text = "Compose CodeLab 화이팅!")
+        BodyContent(Modifier.padding(innerPadding))
     }
 }
 @Composable
@@ -91,7 +89,7 @@ fun PhotographerCard(modifier: Modifier = Modifier){
             .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colors.surface)
             .padding(16.dp) //padding을 두번 선언하는 것은 이 카드와 밖의 레이아웃의 padding
-            ){
+    ){
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -103,7 +101,7 @@ fun PhotographerCard(modifier: Modifier = Modifier){
             modifier = Modifier
                 .padding(start = 8.dp)
                 .align(Alignment.CenterVertically)
-                ){
+        ){
             Text("정 자이노", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text("3 분전", style = MaterialTheme.typography.body2)
@@ -156,10 +154,164 @@ fun ImageList(){
         }
     }
 }
+fun Modifier.firstBaseToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout{ measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height){
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content : @Composable () -> Unit
+){
+    Layout(content = content, modifier = modifier,
+    ){measurables, constraints ->
+        val placeable = measurables.map{ measurable ->
+            measurable.measure(constraints)
+        }
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight){
+            placeable.forEach { placeable ->
+                placeable.placeRelative(x = 0 , y = yPosition)
+
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+@Composable
+fun BodyContent(modifier : Modifier = Modifier){
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray, shape = RectangleShape)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
+        StaggeredGrid(modifier = modifier) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+@Composable
+fun StaggeredGrid(
+    modifier :Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+){
+    Layout(modifier = modifier,
+        content = content
+    ){ measurables, constraints ->
+
+        val rowWidths = IntArray(rows){ 0 }
+        val rowHeights = IntArray(rows){ 0 }
+
+        val placeables = measurables.mapIndexed{ index, measurable ->
+
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf{ it}
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows){ 0 }
+        for (i in 1 until rows){
+            rowY[i] = rowY[i - 1] + rowHeights[i-1]
+        }
+        layout(width, height){
+            val rowX = IntArray(rows){ 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+@Composable
+fun Chip(modifier : Modifier = Modifier, text: String){
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+@Stable
+fun Modifier.padding(all: Dp) =
+    this then(
+            PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
+    )
+private class PaddingModifier(
+    val start: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val end: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val rtlAware: Boolean = true
+) : LayoutModifier {
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+        return layout(width, height){
+            if(rtlAware){
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            }else {
+                placeable.place(start.roundToPx(), top.roundToPx())
+            }
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        ImageList()
+        BodyContent()
     }
 }
