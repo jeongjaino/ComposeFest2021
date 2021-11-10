de1dff6808bc54d3c781847a7d3031cdfe35c475
jaino <msb3302@naver.com>
2021-11-10 14:21:17 +0900

    List, LazyColumn, ImageList

--- week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,39 +3,90 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-               PhotographerCard()
+                ImageList()
             }
         }
     }
 }
 @Composable
+fun SimpleList(){
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item $it")
+        }
+    }
+}
+@Composable
+fun LazyList(){
+
+    val scrollState = rememberLazyListState()
+    
+    LazyColumn(state = scrollState){
+        items(100){
+            Text("Item $it")
+        }
+    }
+}
+@Composable
+fun LayoutCodeLab(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodeLab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+           BodyContent(Modifier.padding(innerPadding))
+    }
+}
+@Composable
+fun BodyContent(modifier : Modifier = Modifier ){
+    Column(modifier = modifier.padding(8.dp)){
+        Text(text = "안녕하세요!")
+        Text(text = "Compose CodeLab 화이팅!")
+    }
+}
+@Composable
 fun PhotographerCard(modifier: Modifier = Modifier){
     Row (
-        modifier.padding(16.dp)
+        modifier
+            .padding(16.dp)
             .clickable(onClick = {})
             .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colors.surface)
@@ -60,10 +111,55 @@ fun PhotographerCard(modifier: Modifier = Modifier){
         }
     }
 }
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+@Composable
+fun ImageList(){
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row() {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("위로 올리기123123")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("아래로 내리기123123")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        PhotographerCard()
+        ImageList()
     }
 }
