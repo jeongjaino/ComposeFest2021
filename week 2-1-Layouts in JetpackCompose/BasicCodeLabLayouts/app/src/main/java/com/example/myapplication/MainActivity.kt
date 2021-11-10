c1dbb330bb4513d5341283566a9ecb35d7a96a63
jaino <msb3302@naver.com>
2021-11-10 16:54:40 +0900

    button, text in ConstraintLayout

--- week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/BasicCodeLabLayouts/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,6 +26,9 @@ import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.rememberImagePainter
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.launch
@@ -35,7 +38,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                BodyContent()
+                Surface{
+                    TwoTexts(text1 = "Hi", text2 ="Jaino" )
+                }
             }
         }
     }
@@ -281,6 +286,7 @@ fun Modifier.padding(all: Dp) =
     this then(
             PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
     )
+
 private class PaddingModifier(
     val start: Dp = 0.dp,
     val top: Dp = 0.dp,
@@ -308,10 +314,99 @@ private class PaddingModifier(
         }
     }
 }
+@Composable
+fun ConstraintLayoutContent(){
+    ConstraintLayout{
+
+        val (button, text) = createRefs()
+
+        Button(
+            onClick = {},
+            modifier = Modifier.constrainAs(button){
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ){
+            Text("Button")
+        }
+        Text("Text", Modifier.constrainAs(text){
+            top.linkTo(button.bottom, margin = 16.dp)
+            centerHorizontallyTo(parent)
+        })
+    }
+}
+@Composable
+fun LargeConstraintLayout(){
+    ConstraintLayout{
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            "This is a looooooooooooooooooooooooooooooong text",
+            Modifier.constrainAs(text){
+                linkTo(start = guideline, end = parent.end)
+                width = Dimension.preferredWrapContent
+            }
+        )
+    }
+}
+@Composable
+fun DecoupledConstraintLayout(){
+    BoxWithConstraints() {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = {},
+                modifier = Modifier.layoutId("button")
+            ){
+                Text("button")
+            }
+            Text("text", Modifier.layoutId("text"))
+        }
+    }
+}
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button){
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text){
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+@Composable
+fun TwoTexts(modifier : Modifier = Modifier, text1: String, text2: String){
+    Row(modifier = modifier.height(IntrinsicSize.Min)){
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        BodyContent()
+        DecoupledConstraintLayout()
     }
 }
